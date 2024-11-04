4b69b23b32edb6a917b03ee4932685caf764dcc3
SidharthSrinivasan <sids6501@gmail.com>
2024-11-04 16:05:15 -0700

    Add data validation to kafka_consumer.py

--- data_streaming/kafka_consumer.py
+++ data_streaming/kafka_consumer.py
@@ -1,9 +1,10 @@
-
+import pandas as pd
 import psycopg2
 from kafka import KafkaConsumer
 import json
+from data_validation.validate import validate_transaction_data
 
-# Connect to PostgreSQL
+# Connect to PostgresSQL
 conn = psycopg2.connect(
     dbname="synthetic_data",
     user="postgres",
@@ -24,13 +25,26 @@ for message in consumer:
     # Assume the message is a dictionary with keys matching the database columns
     data = json.loads(message.value.decode('utf-8'))
 
-    cursor.execute(
-        """
-        INSERT INTO transactions (transaction_id, user_id, product, quantity, price, timestamp)
-        VALUES (%s, %s, %s, %s, %s, %s)
-        """,
-        (data["transaction_id"], data["user_id"], data["product"], data["quantity"], data["price"], data["timestamp"])
-    )
+    # Create dataframe for consumed data
+    transaction_data = pd.DataFrame([data])
+
+    # Check for validation errors in consumed data
+    validation_errors = validate_transaction_data(transaction_data)
+
+    # Print all validation errors if any
+    if validation_errors:
+        for error in validation_errors:
+            print(error)
+    # Insert to table if no validation error
+    else:
+        cursor.execute(
+            """
+            INSERT INTO transactions (transaction_id, user_id, product, quantity, price, timestamp)
+            VALUES (%s, %s, %s, %s, %s, %s)
+            """,
+            (data["transaction_id"], data["user_id"], data["product"], data["quantity"], data["price"],
+             data["timestamp"])
+        )
     conn.commit()
 
 # Close connection
